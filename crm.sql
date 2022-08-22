b9482aca3ba1c62ddf813ad235cd94e0b8742614
suvamita <suvamitasadhukhan@gmail.com>
2022-08-22 17:28:00 +0530

    CRM

---
+++ crm.sql
@@ -0,0 +1,209 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 22, 2022 at 12:45 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prequest`
+--
+
+CREATE TABLE `prequest` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `contactno` varchar(11) DEFAULT NULL,
+  `company` varchar(255) DEFAULT NULL,
+  `wdd` varchar(255) DEFAULT NULL,
+  `cms` varchar(255) DEFAULT NULL,
+  `seo` varchar(255) DEFAULT NULL,
+  `smo` varchar(255) DEFAULT NULL,
+  `swd` varchar(255) DEFAULT NULL,
+  `dwd` varchar(255) DEFAULT NULL,
+  `fwd` varchar(255) DEFAULT NULL,
+  `dr` varchar(255) DEFAULT NULL,
+  `whs` varchar(255) DEFAULT NULL,
+  `wm` varchar(255) DEFAULT NULL,
+  `ed` varchar(255) DEFAULT NULL,
+  `wta` varchar(255) DEFAULT NULL,
+  `opi` varchar(255) DEFAULT NULL,
+  `ld` varchar(255) DEFAULT NULL,
+  `da` varchar(255) DEFAULT NULL,
+  `osc` varchar(255) DEFAULT NULL,
+  `nd` varchar(255) DEFAULT NULL,
+  `others` varchar(255) DEFAULT NULL,
+  `query` longtext DEFAULT NULL,
+  `posting_date` date DEFAULT NULL,
+  `remark` longtext DEFAULT NULL,
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `prequest`
+--
+
+INSERT INTO `prequest` (`id`, `name`, `email`, `contactno`, `company`, `wdd`, `cms`, `seo`, `smo`, `swd`, `dwd`, `fwd`, `dr`, `whs`, `wm`, `ed`, `wta`, `opi`, `ld`, `da`, `osc`, `nd`, `others`, `query`, `posting_date`, `remark`, `status`) VALUES
+(1, 'Anuj Kumar', 'phpgurukulteam@gmail.com', '1234567890', 'Test', 'Website Design & Development', '', '', '', '', 'Dynamic Website Design', '', '', 'Web Hosting Services', '', 'Ecommerce Development', 'Walk Through Animation', '', '', '', '', '', '', 'This is for testing', '2021-04-22', 'This is for test', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id` int(11) NOT NULL,
+  `ticket_id` varchar(11) DEFAULT NULL,
+  `email_id` varchar(300) DEFAULT NULL,
+  `subject` varchar(300) DEFAULT NULL,
+  `task_type` varchar(300) DEFAULT NULL,
+  `prioprity` varchar(300) DEFAULT NULL,
+  `ticket` longtext DEFAULT NULL,
+  `attachment` varchar(300) DEFAULT NULL,
+  `status` varchar(300) DEFAULT NULL,
+  `admin_remark` longtext DEFAULT NULL,
+  `posting_date` date DEFAULT NULL,
+  `admin_remark_date` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ticket`
+--
+
+INSERT INTO `ticket` (`id`, `ticket_id`, `email_id`, `subject`, `task_type`, `prioprity`, `ticket`, `attachment`, `status`, `admin_remark`, `posting_date`, `admin_remark_date`) VALUES
+(13, '7', 'sadhukhan@gmail.com', 'Repairing', 'billing', 'important', 'Computer Repairing.', NULL, 'Open', NULL, '2022-08-22', '2022-08-22 10:40:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `alt_email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `address` varchar(500) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `posting_date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `alt_email`, `password`, `mobile`, `gender`, `address`, `status`, `posting_date`) VALUES
+(3, 'Suvamita Sadhukhan', 'suvamita@gmail.com', NULL, '123', '6291169400', 'f', NULL, NULL, '2022-08-22 10:31:28'),
+(2, 'Suvamita Sadhukhan', 'suvamitasadhukhan@gmail.com', NULL, '12', '6291169462', 'm', NULL, NULL, '2022-08-22 10:03:57'),
+(4, 'S Sadhukhan', 'sadhukhan@gmail.com', NULL, '89', '6291169462', 'm', NULL, NULL, '2022-08-22 10:37:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usercheck`
+--
+
+CREATE TABLE `usercheck` (
+  `id` int(11) NOT NULL,
+  `logindate` varchar(255) DEFAULT '',
+  `logintime` varchar(255) DEFAULT '',
+  `user_id` int(11) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT '',
+  `ip` varbinary(16) DEFAULT NULL,
+  `mac` varbinary(16) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `usercheck`
+--
+
+INSERT INTO `usercheck` (`id`, `logindate`, `logintime`, `user_id`, `username`, `email`, `ip`, `mac`, `city`, `country`) VALUES
+(4, '2022/08/22', '04:08:06pm', 4, 'S Sadhukhan', 'sadhukhan@gmail.com', 0x3a3a31, 0x43412d39342d30322d31302d34462d46, '', ''),
+(3, '2022/08/22', '03:34:07pm', 2, 'Suvamita Sadhukhan', 'suvamitasadhukhan@gmail.com', 0x3a3a31, 0x43412d39342d30322d31302d34462d46, '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `prequest`
+--
+ALTER TABLE `prequest`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usercheck`
+--
+ALTER TABLE `usercheck`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `prequest`
+--
+ALTER TABLE `prequest`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `usercheck`
+--
+ALTER TABLE `usercheck`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRM</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <!-- <link rel="stylesheet" href="design.css"> -->
+    <link rel="stylesheet" href="design2.css">
+</head>
+
+<body>
+
+    <section class="head">
+
+        <header>
+            <a href="#"><img src="logoo.jpg" alt="Micro" class="logo"></a>
+            <!-- <div class="toggle" onclick="togglemenu"></div> -->
+            <ul id="nav" class="navigation">
+                <li><a class="navigation" href="registration.php">User Sign Up</a></li>
+                <li><a class="navigation" href="login.php">User Log In</a></a></li>
+                <!--<li><a class="navigation" href="admin/">Admin</a></li>-->
+                <!--<li><a href="">Register</a></li>-->
+            </ul>
+
+        </header>
+        <div class="content">
+            <div class="text">
+                <h2>Micro Company CRM<br>The Finest <span>Fintech</span> Startup</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dignissimos ipsa iure fugit, voluptatibus suscipit consectetur quisquam neque vel facilis.</p>
+                <a href="#">Learn More</a>
+            </div>
+            <div class="imgBox">
+                <img src="pic7.png" alt="CRM" class="crm">
+            </div>
+        </div>
+        <!-- <ul class="sci">
+            <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+            
+            <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+        </ul> -->
+    </section>
+    <!-- <script>
+        function togglemenu(){
+            var menuToggle = document.querySelector('.toggle');
+            var navigation = document.querySelector('.navigation');
+            menuToggle.classList.toggle('active')
+            navigation.classList.toggle('active')
+        }
+    </script> -->
+</body>
+
+</html>
